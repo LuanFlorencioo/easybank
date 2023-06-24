a0466c9d6a4c3aef517a38431710994c60c355b7
Luan Florencio <luanflorencio.dev@gmail.com>
2023-06-24 13:09:51 -0300

    feat: add initial config

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,17 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import { Provider } from "react-redux";
+import { store } from "@/app";
+import { font } from "@/styles";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+const App = ({ Component, pageProps }: AppProps) => {
+  return (
+    <Provider store={store}>
+      <div className={font}>
+        <Component {...pageProps} />
+      </div>
+    </Provider>
+  )
 }
+
+export default App;
