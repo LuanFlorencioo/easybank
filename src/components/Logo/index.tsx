e747f2154d4a5376bc3c13498bbc21b83e0c7ad1
Luan Florencio <luanflorencio.dev@gmail.com>
2023-06-24 14:32:44 -0300

    feat: create Logo component

---
+++ src/components/Logo/index.tsx
@@ -0,0 +1,8 @@
+import Image from "next/image";
+import logo from "/public/logo.svg";
+
+const Logo = () => {
+  return <Image src={logo} alt="Easybank Logo"/>
+}
+
+export default Logo;
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,9 +1,11 @@
 import Title from "./Title";
 import Text from "./Text";
 import Button from "./Button";
+import Logo from "./Logo";
 
 export {
   Title,
   Text,
   Button,
+  Logo,
 }
