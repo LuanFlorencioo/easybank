aa4e6da5f25f2cd2509ae83fc74ed5b2a76403ca
Luan Florencio <luanflorencio.dev@gmail.com>
2023-06-24 20:59:17 -0300

    feat: add Header in app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,14 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { font } from "@/styles";
+import { Header } from "@/components";
 import { AppProvider } from "@/contexts";
 
 const App = ({ Component, pageProps }: AppProps) => {
   return (
     <div className={font}>
       <AppProvider>
+        <Header />
         <Component {...pageProps} />
       </AppProvider>
     </div>
